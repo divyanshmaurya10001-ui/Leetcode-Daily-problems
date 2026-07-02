5abcf020aa0883707e79905f96bd66840d423a4c
Divyansh <divyanshmaurya10001@gmail.com>
2026-07-03 00:08:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x < 2)
-            return x;
+        if(x<2)
+        return x ;//iska mtlb hai ki 2 se chota 1 aur 0 hota hai toh hame uska square root nikalne ki jarurat nahi hoti kui woh khud hi sqrt hote hai khud k 
 
-        int low = 1, high = x / 2;
+        int low = 1 , high = x/2;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        //high ko x/2 issliye kiya kyu hi jo bhi sqrt value ki range hogi woh 0 se x/ k buch hi hogi 
+        // dry run : x=16
+        //          sqrt(16)=4
+        //          aur x/2 = 8 mtlb ki 4 jo hai 1 sr 8 bich hi rahega 
 
-            if (mid == x / mid)
+        while(low <= high ){
+            int mid = low +(high - low )/2;
+            if(mid == x/mid){
                 return mid;
-            else if (mid < x / mid)
+            }
+            else if(mid < x/mid){
                 low = mid + 1;
+            }
             else
-                high = mid - 1;
+               high = mid -1;
+        
         }
-
-        return high;
+        return high ;//When the exact square root doesn't exist, high stores the largest integer whose square is less than x.
     }
 };
