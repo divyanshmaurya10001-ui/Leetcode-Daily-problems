99bc533ca1e616650e27bb63c6dd2bf5e1f2ad42
Divyansh <divyanshmaurya10001@gmail.com>
2026-07-05 10:15:10 +0530

    Time: 3 ms (34.62%), Space: 62.9 MB (48.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,8 +6,9 @@ public:
         int ans = 0 ;
 
         while(low <= high){
-            int area = min(height[low],height[high])*(high-low);
-             ans = max(ans,area);
+            int area = min(height[low],height[high])*(high-low);//yaha pr min(height[low],height[high]) yeh iss liye likha hai isliye kyu ki maan lo ek ki height 8 hai aur dusre hi height 7 hai tooh hame 7 wali heieght ko concider warna hoga water overflow kr jayega toh fir maximum area nahi le payenge 
+             
+              ans = max(ans,area);
             if(height[low]<height[high]){
                 low++;
               }
