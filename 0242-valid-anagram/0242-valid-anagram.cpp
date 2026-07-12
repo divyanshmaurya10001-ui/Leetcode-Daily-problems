7c8a4b89c295579b91b34a10ba56ce2bc8be62ef
Divyansh <divyanshmaurya10001@gmail.com>
2026-07-13 02:17:41 +0530

    Time: 4 ms (34.88%), Space: 9.5 MB (92.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,7 +5,12 @@ public:
         int hash[26]={0};
         for(int i=0;i<s.size();i++){
             hash[s[i]-'a']++;
-            hash[t[i]-'a']--;
+             hash[t[i]-'a']--;
+             //iska mtlb hota hai ki ki pehel hum ek hash array define karnege aur fir ek ek kr value usme jalunge 1 st string ka aur fir hum ek ek kr hash array se element nikalenge jo bhi match kr jayenge  
+            
+            // aur 'a' se sub iss liye kiya hai kyu ki index fix rahe 
+            //aur Index:  0 1 2 3 4 5 ...
+                //Value:  0 0 0 0 0 0 ... mtlb jo isme hash[26]={0}hai yeh batat hai ki kisi element ki kitni frequnecy hai 
         }
         for(int i=0;i<26;i++){
             if(hash[i]!=0){
